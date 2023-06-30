1b53f4dadddccc1bdb9a3d87fb7c4d7ac411d3ad
akashp28 <akashkarun28>
2023-06-30 10:52:39 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import './Home.css'
 import img1 from '../assets/img1.png'
+import { useState } from 'react'
+ 
 
 const Home = () => {
+  let yellow = 'linear-gradient(#fb5353, #feba45)';
+  const [bgColor, setBgColor] = useState(yellow);
+  const [count,setcount]= useState(0);
+   const changeColor =()=>{
+    setBgColor((prevColor) => {
+      return prevColor === yellow ? 'black' : yellow;
+    });
+    setcount(count+1)
+    }
   return (
-    <div className="container">
+    <div className="container" style={{background: bgColor}}>
         <div className="header">
             <div className="h1">
     <ul>
-    <img src={img1} className='logo'/>
+    <img src={img1} className='logo'/> 
         </ul>
         </div>
         <ul>
         <li><a href="#premium">Premium</a></li>
         <li><a href="#help">Help</a></li>
         <li><a href="#download">Download</a></li>
-        <li><a href="##">|</a></li>
+        <li><a>|</a></li>
         <li><a href="#signup">Sign up</a></li>
         <li><a href="#login">Login</a></li>
       </ul>
@@ -24,6 +35,8 @@ const Home = () => {
         <div className='info1'>Music for EveryOne</div>
         <div className='info2'>Millions of Songs. No Credit Card Needed</div>
         <input type="button" className='btn1' value="GET SPOTIFY FREE" />
+        <button className='btn2' onClick={changeColor}>Change Theme</button>
+        <span>Button Clicked {count} Time(s)</span>
     </div>
     </div>
    
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import {React  , useState }from 'react'
+import {React  , useState ,useEffect }from 'react'
 import './Login.css'
 
 const Login = () => {
@@ -11,22 +11,30 @@ const Login = () => {
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+    
   };
+
+  const[value,setValue]=useState(0);
+       
+
+
+  useEffect(()=> {console.log(value) ;},[value]) 
   return (
    
     <div className='container1'>
       <form action="#">
       <div className='login'>
-            <h3>Login</h3>
+            <h3 className='log'>Login</h3>
            <label htmlFor="email">Email :</label>
-           <input type="email" id="email" placeholder="Enter Email" required></input>
-       <label htmlFor="password">Password :</label>
-       <input type={passwordVisible ? 'text' : 'password'}value={password} onChange={handlePasswordChange} placeholder='Enter Password' required />
+           <input type="email" id="email" placeholder="Enter Email" required onChange={(e)=>console.log(e.target.value)} ></input>
+       <label htmlFor="password" >Password :</label>
+       <input type={passwordVisible ? 'text' : 'password'}value={password} onChange={handlePasswordChange} placeholder='Enter Password' required id='pass' />
        <div className='chkbox'>
        <input type="checkbox"  checked={passwordVisible}onChange={handlePasswordToggle} id='checkbox'/> 
        <label id='sh'>Show Password </label>
        </div>
-           <input type="submit" value="Sign in" id="s1" ></input>
+       <button onClick={()=>setValue(document.getElementById('pass').value)} id='s1'>Sign In</button>
+           
         <div className='div3'>
           Forgot <a href="">Username / Password</a> ?<br />
           Don't have an Account ? <a href="">Sign up</a>
